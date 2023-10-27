6d5effbf3b4d694cb8f5769748f20b82656e9e92
Nester44 <nesterov.mail4@gmail.com>
2023-10-27 15:55:54 +0200

    add implementation of tokenizeInput & getTokenType
    authored-by: Martynenko Oleh <o.martynenko.im11@kpi.ua>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log(123)
+const operands = ['+', '-', '/', '*']
+
+type TokenType = 'number' | 'operand' | 'eqsign'
+
+const singleNumberRegExp = /^[0-9]$/
+
+export function getTokenType(value: string): TokenType {
+	if (operands.includes(value)) return 'operand'
+	else if (value === '=') return 'eqsign'
+	else if (value.match(singleNumberRegExp)) return 'number'
+	else throw new Error('Invalid value in file')
+}
+
+export function tokenizeInput(input: string): Token[] {
+	if (!input) return []
+
+	const tokens: Token[] = []
+
+	const inputValues: string[] = input.split(' ')
+
+	for (const value of inputValues) {
+		const type = getTokenType(value)
+
+		tokens.push({ value, type })
+	}
+	return tokens
+}
+
+export type Token = {
+	value: string
+	type: TokenType
+}
